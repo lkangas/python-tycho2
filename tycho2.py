2d57e084c850240e4812d0189828924c0809df1e
lkangas <vostok@iki.fi>
2017-10-22 20:18:45 +0300

    Add tycho2 class

---
+++ tycho2.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 22 12:42:31 2017
+
+@author: lauri.kangas
+"""
+
+import numpy as np
+import gsc_regions
+import coordinate_transformations as coord
+import projections
+
+class tycho2:
+    def __init__(self, index_filename, catalog_filename, supplement_filename, \
+                 projection=None):
+        self._index = np.load(index_filename)
+        self._catalog = np.load(catalog_filename, mmap_mode='r')
+        self._supplement = np.load(supplement_filename)
+        if projection:
+            self._projection = projection
+        else:
+            self._projection = projections.stereographic
+        
+    def stars_in_fov(self, center_RADEC, fov, rotation, LM=6):
+        
+        regions, = gsc_regions.find_GSC_regions(self._index, \
+                                                center_RADEC, \
+                                                rotation, \
+                                                fov, \
+                                                projection=self._projection, \
+                                                area='rectangle')
+        
+        # GSC index has an extra row in the end (index 9537) marked at RADEC 0,0. filter it out.
+        regions = regions[regions < self._index.shape[0]-1]
+        
+        tyc_start_inds = self._index[:,0] # starting indices for tycho2 stars in GSC region
+        sup_start_inds = self._index[:,1] #  ... for tycho2 supplement stars
+        
+        tycho_ranges = [np.arange(tyc_start_inds[i], tyc_start_inds[i+1]) for i in regions]
+        supplement_ranges = [np.arange(sup_start_inds[i], sup_start_inds[i+1]) for i in regions]
+        
+        tycho_indices = np.concatenate(tycho_ranges).astype('int')
+        suppl_indices = np.concatenate(supplement_ranges).astype('int')
+        
+        # append supplement to catalog, making an Nx3 array (ra, dec, mag)
+        supplemented_catalog = np.vstack([self._catalog[tycho_indices], \
+                                          self._supplement[suppl_indices]])
+    
+        # strip out stars below LM
+        mags = supplemented_catalog[:,2]
+        trimmer_catalog = supplemented_catalog[mags < LM]
+        
+        RAs = trimmer_catalog[:,0]
+        DECs = trimmer_catalog[:,1]
+        mags = trimmer_catalog[:,2]
+        
+        xyz = coord.rotate_RADEC(RAs, DECs, *center_RADEC)
+        image_x, image_y = coord.xyz_to_imagexy(*xyz, \
+                                                rotation=rotation, \
+                                                projection=self._projection)
+        
+        return image_x, image_y, mags
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
