fc1819b2774ee0523f90ce8b21ffd7b7b21111d4
lkangas <vostok@iki.fi>
2017-10-29 00:25:25 +0300

    Updated example script

--- example.py
+++ example.py
@@ -12,35 +12,35 @@ from tycho2 import tycho2
 from projections import stereographic, unity
 import coordinate_transformations as coord
     
-index = np.load('tyc2index.npy')
-catalog = np.load('tyc2.npy', mmap_mode='r')
-suppl = np.load('tyc2sup.npy')
-
+T = tycho2('tyc2index.npy', 'tyc2.npy', 'tyc2sup.npy')
 center_RADEC = np.radians([90, 0])
 rotation = np.radians(0)
 
-fov_degrees = np.array([70, 70])
+fov_degrees = np.array([23, 5])
 fov_radians = np.radians(fov_degrees)
-half_fov_radians = fov_radians/2
+
+radius = coord.fov_radius(fov_degrees)
 
 projection = stereographic
 projection = unity
 
-image_plane_half_fov = projection(half_fov_radians)
-
-LM = 12
+LM = 9
 factor = 4
 
-TYC2 = tycho2('tyc2index.npy', 'tyc2.npy', 'tyc2sup.npy', projection=projection)
-image_x, image_y, mags = TYC2.stars_in_fov(center_RADEC, fov_radians, rotation, LM=LM)
+regions = T.regions_within_radius(center_RADEC, radius)
+RAs, DECs, mags = T.stars_in_regions(regions, LM=LM)
+
+xyz = coord.rotate_RADEC(RAs, DECs, *center_RADEC)
+image_x, image_y = coord.xyz_to_imagexy(*xyz, rotation=rotation)
 
 plt.clf()
-plt.scatter(image_x, image_y, (LM-mags)**2.5/LM*factor, alpha=1)
+plt.scatter(image_x, image_y, (LM-mags)**2.5/LM*factor)
 
 plt.axis('equal')
 
 import matplotlib.patches as patches
 
+image_plane_half_fov = projection(np.radians(fov_degrees/2))
 plt.gca().add_patch(patches.Rectangle(-image_plane_half_fov, *(2*image_plane_half_fov), fill=False))
 plt.tight_layout()
-plt.axis('off')
+#plt.axis('off')
