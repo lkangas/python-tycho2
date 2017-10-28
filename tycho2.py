544a490d6b6cb0383ffd93f81127621af80eb179
lkangas <vostok@iki.fi>
2017-10-28 23:07:19 +0300

    Refactor by splitting functions
    Split star searching to region searching and getting stars in selected
    regions. Limiting magnitude is applied in stars_in_regions.
    
    Projections should be removed from tycho2.

--- tycho2.py
+++ tycho2.py
@@ -6,7 +6,6 @@ Created on Sun Oct 22 12:42:31 2017
 """
 
 import numpy as np
-import gsc_regions
 import coordinate_transformations as coord
 import projections
 
@@ -20,46 +19,6 @@ class tycho2:
             self._projection = projection
         else:
             self._projection = projections.stereographic
-        
-    def stars_in_fov(self, center_RADEC, fov, rotation, LM=6):
-        
-        regions, = gsc_regions.find_GSC_regions(self._index, \
-                                                center_RADEC, \
-                                                rotation, \
-                                                fov, \
-                                                projection=self._projection, \
-                                                area='rectangle')
-        
-        # GSC index has an extra row in the end (index 9537) marked at RADEC 0,0. filter it out.
-        regions = regions[regions < self._index.shape[0]-1]
-        
-        tyc_start_inds = self._index[:,0] # starting indices for tycho2 stars in GSC region
-        sup_start_inds = self._index[:,1] #  ... for tycho2 supplement stars
-        
-        tycho_ranges = [np.arange(tyc_start_inds[i], tyc_start_inds[i+1]) for i in regions]
-        supplement_ranges = [np.arange(sup_start_inds[i], sup_start_inds[i+1]) for i in regions]
-        
-        tycho_indices = np.concatenate(tycho_ranges).astype('int')
-        suppl_indices = np.concatenate(supplement_ranges).astype('int')
-        
-        # append supplement to catalog, making an Nx3 array (ra, dec, mag)
-        supplemented_catalog = np.vstack([self._catalog[tycho_indices], \
-                                          self._supplement[suppl_indices]])
-    
-        # strip out stars below LM
-        mags = supplemented_catalog[:,2]
-        trimmer_catalog = supplemented_catalog[mags < LM]
-        
-        RAs = trimmer_catalog[:,0]
-        DECs = trimmer_catalog[:,1]
-        mags = trimmer_catalog[:,2]
-        
-        xyz = coord.rotate_RADEC(RAs, DECs, *center_RADEC)
-        image_x, image_y = coord.xyz_to_imagexy(*xyz, \
-                                                rotation=rotation, \
-                                                projection=self._projection)
-        
-        return image_x, image_y, mags
         
     def regions_within_radius(self, center, radius):
         radius = np.radians(radius)
@@ -79,6 +38,40 @@ class tycho2:
         regions = regions[regions < self._index.shape[0]-1]
         
         return regions
+    
+    def stars_in_regions(self, regions, LM=None):
+        tyc_start_inds = self._index[:,0] # starting indices for tycho2 stars in GSC region
+        sup_start_inds = self._index[:,1] #  ... for tycho2 supplement stars
+        
+        tycho_ranges = [np.arange(tyc_start_inds[i], tyc_start_inds[i+1]) for i in regions]
+        supplement_ranges = [np.arange(sup_start_inds[i], sup_start_inds[i+1]) for i in regions]
+        
+        tycho_indices = np.concatenate(tycho_ranges).astype('int')
+        suppl_indices = np.concatenate(supplement_ranges).astype('int')
+        
+        # append supplement to catalog, making an Nx3 array (ra, dec, mag)
+        supplemented_catalog = np.vstack([self._catalog[tycho_indices], \
+                                          self._supplement[suppl_indices]])
+    
+        if LM:
+            # strip out stars below LM
+            mags = supplemented_catalog[:,2]
+            trimmed_catalog = supplemented_catalog[mags < LM]
+        else:
+            trimmed_catalog = supplemented_catalog
+            
+        RAs = trimmed_catalog[:,0]
+        DECs = trimmed_catalog[:,1]
+        mags = trimmed_catalog[:,2]
+        
+        return RAs, DECs, mags
+    
+#        xyz = coord.rotate_RADEC(RAs, DECs, *center_RADEC)
+#        image_x, image_y = coord.xyz_to_imagexy(*xyz, \
+#                                                rotation=rotation, \
+#                                                projection=self._projection)
+        
+        
             
         
         
