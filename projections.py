0a7d2d395d645a6d396656a58d450fe76aa6d1a0
lkangas <vostok@iki.fi>
2017-11-02 19:36:29 +0200

    Add rectilinear projection

--- projections.py
+++ projections.py
@@ -6,6 +6,12 @@ Created on Sat Oct 21 22:29:51 2017
 """
 import numpy as np
 
+def rectilinear(x, inverse=False):
+    if inverse:
+        return np.arctan(x)
+    else:
+        return np.tan(x)
+
 def stereographic(x, inverse=False):
     if inverse: # mm (image plane) to radians (celestial sphere)
         return 2*np.arctan(x/2)
